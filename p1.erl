57eba71fe7140c24c61dbbe0a9f7c5de4b7fe1e9
Ram Gole <ram@ram-laptop.(none)>
2011-11-18 12:36:06 +0530

    p2 solved p1 is pending

---
+++ p1.erl
@@ -0,0 +1,16 @@
+-module(p2).
+-export([second_last/1]).
+-include_lib("eunit/include/eunit.hrl").
+
+second_last([]) -> nil;
+second_last([_|[]]) -> nil;
+second_last([X,_|[]]) -> X;
+second_last([_|T]) -> second_last(T).
+
+
+second_last_test() ->
+  ?assertEqual(nil, second_last([])),
+  ?assertEqual(nil, second_last([1])),
+  ?assertEqual(2, second_last([2,1])),
+  ?assertEqual(2, second_last([3,2,1])),
+  ?assertEqual(2, second_last([4,3,2,1])).
